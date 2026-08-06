ff641a805f90140d7484d2c42caea8892d1e30b3
Girardin <vincent.girardin@berner.fr>
2026-08-06 12:16:38 +0200

    fixed cicd and tests

--- tests/test_api_performance.py
+++ tests/test_api_performance.py
@@ -2,10 +2,10 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 from src.main import app
 
+
 @pytest.mark.asyncio
-async def test_async_predict_endpoint():
+async def test_async_predict_endpoint(valid_payload):  # Passer le fixture valide
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as client:
-        response = await client.post("/predict", json={"code_client": "1500000900", "effectif": 3.0})
+        response = await client.post("/predict", json=valid_payload)
         assert response.status_code == 200
-        assert "score" in response.json()
